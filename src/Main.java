162ccf89c7a696e4d3a6272cd6085dc4b2034df4
yermolenkoia <tobt@ukr.net>
2017-04-07 16:55:49 +0300

    commit new

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,12 @@ public class Main {
         System.out.println(markovChain.getCurentPobability(8));
 		System.out.println("hello, world")
     }
+	public void testFunc(){
+		Double doubNum = new Double(19.0)
+		while (false){
+			int i = 0;
+			i++
+		}
+		System.out.println("teeeeest")
+	}
 }
