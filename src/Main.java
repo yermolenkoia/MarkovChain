9a1316dc81cd37df25988d4a3a53902080ef1e84
yermolenkoia <tobt@ukr.net>
2017-04-07 16:52:26 +0300

    new Func added

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,11 @@ public class Main {
         System.out.println(markovChain.getCurentPobability(8));
 		System.out.println("hello, world")
     }
+	public void testFunc)(){
+		Integer newInt = new Integer(1);
+		System.out.println("hello, world")
+		if (true){
+			System.out.println(true)
+		}
+	}
 }
